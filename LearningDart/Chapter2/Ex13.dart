b93a4b8b71b3481ac24c77a7bf06fe158e140ffa
Nguyễn Khánh <150260593+khnhk0ogei04@users.noreply.github.com>
2025-02-17 22:11:37 +0700

    Create Ex13.dart

---
+++ LearningDart/Chapter2/Ex13.dart
@@ -0,0 +1,59 @@
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: GridViewScreen()
+    );
+  }
+}
+
+class GridViewScreen extends StatelessWidget{
+  final List<String> items = List.generate(20, (index) => "This is ${index + 1}");
+  @override
+  Widget build (BuildContext context){
+    return Scaffold(
+        appBar: AppBar(
+            title: Text("GridView Example")
+        ),
+        body: GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10
+            ),
+            itemCount: items.length,
+            itemBuilder: (BuildContext context, int index){
+              return GestureDetector(
+                onTap: (){
+                  showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                        title: Text("Item selected"),
+                        content: Text("You tapped on ${items[index]}"),
+                        actions: [
+                          TextButton(
+                              onPressed: () => Navigator.pop(context),
+                              child: Text("OKAY")
+                          )
+                        ],
+                      )
+                  );
+                },
+                child: Container(
+                    margin: EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                    color: Colors.blueAccent,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  alignment: Alignment.center,
+                  child: Text(
+                    items[index],
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  )
+                ),
+              );
+            })
+    );
+  }
+}
